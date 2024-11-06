b256dbe23204c46b5d76ba8ccc98a30c7791c660
Laurent Perroteau <l.perroteau@humansmatter.co>
2024-11-05 22:51:52 -0500

    Test "All context should implement an hexagonal architecture" (allowing application using primary DTOs)

--- src/HexagonalArchTest.spec.ts
+++ src/HexagonalArchTest.spec.ts
@@ -6,10 +6,11 @@ import { SharedKernel } from './SharedKernel';
 import { BusinessContext } from './BusinessContext';
 
 describe('HexagonalArchTest', () => {
+  const ignoredFiles = ['**/*module.ts', '**/main.ts'];
+
   const srcProject = new TypeScriptProject(
     RelativePath.of('src'),
-    '**/*FilesToExclude*',
-    '**/*OtherFilesToExclude*',
+    ...ignoredFiles,
   );
 
   const sharedKernels = packagesWithContext(SharedKernel.name);
@@ -28,38 +29,39 @@ describe('HexagonalArchTest', () => {
       .map((typeScriptClass) => typeScriptClass.packagePath.getDotsPath());
   }
 
-  // describe('BoundedContexts', () => {
-  //   it.each([...sharedKernels, ...businessContexts])(
-  //     'Should %s not depend on other bounded context domains',
-  //     (context) => {
-  //       noClasses()
-  //         .that()
-  //         .resideInAnyPackage(context + '..')
-  //         .should()
-  //         .dependOnClassesThat()
-  //         .resideInAnyPackage(...otherBusinessContextsDomains(context))
-  //         .because(
-  //           'Contexts can only depend on classes in the same context or shared kernels',
-  //         )
-  //         .check(srcProject.allClasses());
-  //     },
-  //   );
-  // TODO sur HM not Typescript but internal and file with prefix InternalAdapter.ts
-  //   it('primary TypeScript Adapters should only be called from secondaries', () => {
-  //     classes()
-  //       .that()
-  //       .resideInAPackage('..primary..')
-  //       .and()
-  //       .haveSimpleNameStartingWith('TypeScript')
-  //       .should()
-  //       .onlyHaveDependentClassesThat()
-  //       .resideInAPackage('..secondary..')
-  //       .because(
-  //         "To interact between two contexts, secondary from context 'A' should call a primary TypeScript adapter (naming convention starting with 'TypeScript') from context 'B'",
-  //       )
-  //       .check(srcProject.allClasses());
-  //   });
-  // });
+  describe('BoundedContexts', () => {
+    // todo need task have domain/app/infra
+    // it.each([...sharedKernels, ...businessContexts])(
+    //   'Should %s not depend on other bounded context domains',
+    //   (context) => {
+    //     noClasses()
+    //       .that()
+    //       .resideInAnyPackage(context + '..')
+    //       .should()
+    //       .dependOnClassesThat()
+    //       .resideInAnyPackage(...otherBusinessContextsDomains(context))
+    //       .because(
+    //         'Contexts can only depend on classes in the same context or shared kernels',
+    //       )
+    //       .check(srcProject.allClasses());
+    //   },
+    // );
+    // TODO sur HM not Typescript but internal and file with prefix InternalAdapter.ts
+    //   it('primary TypeScript Adapters should only be called from secondaries', () => {
+    //     classes()
+    //       .that()
+    //       .resideInAPackage('..primary..')
+    //       .and()
+    //       .haveSimpleNameStartingWith('TypeScript')
+    //       .should()
+    //       .onlyHaveDependentClassesThat()
+    //       .resideInAPackage('..secondary..')
+    //       .because(
+    //         "To interact between two contexts, secondary from context 'A' should call a primary TypeScript adapter (naming convention starting with 'TypeScript') from context 'B'",
+    //       )
+    //       .check(srcProject.allClasses());
+    //   });
+  });
 
   describe('Domain', () => {
     it('Should not depend on outside', () => {
@@ -68,36 +70,44 @@ describe('HexagonalArchTest', () => {
         .resideInAPackage('..domain..')
         .should()
         .onlyDependOnClassesThat()
-        .resideInAnyPackage('..domain..', ...sharedKernels)
+        .resideInAnyPackage(
+          '..domain..',
+          'node_modules.@nestjs.common..',
+          ...sharedKernels,
+        )
         .because(
           'Domain model should only depend on domains and a very limited set of external dependencies',
         )
         .check(srcProject.allClasses());
     });
 
-    // it.each([...sharedKernels, ...businessContexts])(
-    //   'should be an hexagonal architecture in context %s',
-    //   (context) => {
-    //     Architectures.layeredArchitecture()
-    //       .consideringOnlyDependenciesInAnyPackage(context + '..')
-    //       .withOptionalLayers(true)
-    //       .layer('domain models', context + '.domain..')
-    //       .layer('domain services', context + '.domain..')
-    //       .layer('application services', context + '.application..')
-    //       .layer('primary adapters', context + '.infrastructure.primary..')
-    //       .layer('secondary adapters', context + '.infrastructure.secondary..')
-    //       .whereLayer('application services')
-    //       .mayOnlyBeAccessedByLayers('primary adapters')
-    //       .whereLayer('primary adapters')
-    //       .mayNotBeAccessedByAnyLayer()
-    //       .whereLayer('secondary adapters')
-    //       .mayNotBeAccessedByAnyLayer()
-    //       .because(
-    //         'Each bounded context should implement an hexagonal architecture',
-    //       )
-    //       .check(srcProject.allClasses());
-    //   },
-    // );
+    it.each([...sharedKernels, ...businessContexts])(
+      'should be an hexagonal architecture in context %s',
+      (context) => {
+        Architectures.layeredArchitecture()
+          .consideringOnlyDependenciesInAnyPackage(context + '..')
+          .withOptionalLayers(true)
+          .layer('domain models', context + '.domain..')
+          .layer('domain services', context + '.domain..')
+          .layer('application services', context + '.application..')
+          .layer('primary adapters', context + '.infrastructure.primary..')
+          .layer('secondary adapters', context + '.infrastructure.secondary..')
+          // It's important to ignore .module.ts files
+          .whereLayer('application services')
+          .mayOnlyBeAccessedByLayers('primary adapters')
+          .whereLayer('primary adapters')
+          /**
+           * ⚠️ Was .mayNotBeAccessedByAnyLayer() but I want to allow using primary dto in application)
+           */
+          .mayOnlyBeAccessedByLayers('application services')
+          .whereLayer('secondary adapters')
+          .mayNotBeAccessedByAnyLayer()
+          .because(
+            'Each bounded context should implement an hexagonal architecture',
+          )
+          .check(srcProject.allClasses());
+      },
+    );
   });
 
   /**
